d695fafccf5be583f668754810216dbbd6bf3b9f
thiolliere <guillaume.thiolliere@opmbx.org>
2016-04-09 13:31:12 +0200

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,41 @@
-//! baal is an audio api for games that focus on simplicity.
+//! baal is a cross-plateform audio api for games that focus on simplicity.
 //!
-//! it doesn't provide spatialization.
-//!
-//! feature:
+//! ##Features
 //!
 //! * yaml configuration so you can easily test sounds without recompile
-//! * music player: detail in music mode
-//! * effect player: detail in effect mode
+//! * music player: detail in [music mode](./music/index.html)
+//! * effect player: detail in [effect mode](./effect/index.html)
+//! * no mp3, use ogg vorbis or other format instead
+//! * no spatialization
+//!
+//! for more information about format available see [libsndfile#features](http://www.mega-nerd.com/libsndfile/#features)
+//!
+//! for more information about why not mp3 as lots of other foss handle it see [libsndfile#whynotmp3](http://www.mega-nerd.com/libsndfile/FAQ.html#Q020)
+//!
+//!
+//! ##Dependencies
+//!
+//! * libsndfile:
+//!
+//!   From the website: [libsndfile](http://www.mega-nerd.com/libsndfile/#Download)
+//!
+//!   On Ubuntu / Debian:
+//!   ```sh
+//!   apt-get install libsndfile1-dev
+//!   ```
+//!
+//! * portaudio:
+//!
+//!   rust-portaudio will try to detect portaudio on your system and,
+//!   failing that (or if given the PORTAUDIO\_ONLY\_STATIC environment variable on the build process),
+//!   will download and build portaudio statically.
+//!   If this fails please let us know!
+//!   In the mean-time, you can manually download and install [PortAudio](http://www.portaudio.com/download.html) yourself.
+//!
+//! ##TODO
+//!
+//! * channels conversion
+
 
 //TODO conversion for channels
 
