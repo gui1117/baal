30040829d71b372705137f19ad740e9ac7bb90ba
thiolliere <guillaume.thiolliere@opmbx.org>
2016-04-09 13:00:00 +0200

    doc

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 #baal
 
-easy to use but limited (no spatial sound) audio library
+baal is an audio api for games that focus on simplicity.
 
-name must change
+it doesn't provide spatialization.
 
-almost done. still channels conversion and documentation
+feature:
+
+* yaml configuration so you can easily test sounds without recompile
+* music player: detail in music mode
+* effect player: detail in effect mode
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,12 @@
-//! as part of the amethyst engine, configuration is made in a yaml file.
+//! baal is an audio api for games that focus on simplicity.
 //!
-//! ```yaml
-//! ---
-//! //TODO complete
-//! global_volume: 0.5
-//! music_volume: 0.5
-//! effect_volume: 0.5
+//! it doesn't provide spatialization.
 //!
-//! distance_model: [Linear,32,543]
+//! feature:
 //!
-//! effect:
-//!     - [shoot.ogg,10]
-//!     - [hit.ogg,10]
-//!     - [start.ogg,10]
-//!
-//! music:
-//!     - village.ogg
-//!     - forest.ogg
-//!     - dungeon.ogg
-//! ...
-//! ```
+//! * yaml configuration so you can easily test sounds without recompile
+//! * music player: detail in music mode
+//! * effect player: detail in effect mode
 
 //TODO conversion for channels
 
@@ -37,6 +24,7 @@ use portaudio as pa;
 use std::thread;
 use std::path::{Path, PathBuf};
 use std::ops::Rem;
+use effect::DistanceModel;
 
 static mut RAW_STATE: *mut Mutex<State> = 0 as *mut Mutex<State>;
 
@@ -53,26 +41,73 @@ lazy_static! {
 }
 
 #[derive(Clone,Debug,PartialEq)]
+/// set musics, effects, volumes and audio player.
 pub struct Setting {
-    channels: i32,
-    sample_rate: f64,
-    frames_per_buffer: u32,
+    /// number of channels: 1
+    pub channels: i32,
+    /// sample rate: mostly 44_100
+    pub sample_rate: f64,
+    /// number of frame per buffer: 64
+    pub frames_per_buffer: u32,
 
-    effect_dir: String,
-    music_dir: String,
+    /// the base directory of effects
+    pub effect_dir: String,
+    /// the base directory of musics
+    pub music_dir: String,
 
-    global_volume: f32,
-    music_volume: f32,
-    effect_volume: f32,
+    /// global volume in [0,1]
+    pub global_volume: f32,
+    /// music volume in [0,1]
+    pub music_volume: f32,
+    /// effect volume in [0,1]
+    pub effect_volume: f32,
 
-    distance_model: DistanceModel,
-    music_loop: bool,
+    /// distance model for effect volume computation
+    pub distance_model: DistanceModel,
 
-    effect: Vec<(String,u32)>,
-    music: Vec<String>,
+    /// whereas the music must loop or not
+    pub music_loop: bool,
+
+    /// the list of effect, and number of loading of each, correspond
+    /// to the number of effect playable at the same time
+    /// for example a sword that can be played up to 10 times at the same time ("sword.ogg",10)
+    ///
+    /// each effect is identified by its position in the vector
+    pub effect: Vec<(String,u32)>,
+    /// the list of music
+    ///
+    /// each music is identified by its position in the vector
+    pub music: Vec<String>,
 }
 
 impl Setting {
+    /// import setting from yaml:
+    ///
+    ///```
+    ///---
+    ///channels: 1
+    ///sample_rate: 44100.
+    ///frames_per_buffer: 64
+    ///
+    ///effect_dir: assets/effects
+    ///music_dir: assets/musics
+    ///
+    ///global_volume: 0.5
+    ///music_volume: 0.8
+    ///effect_volume: 0.3
+    ///
+    ///distance_model: [Pow2,10.,110.]
+    ///music_loop: true
+    ///
+    ///effect:
+    ///    - [shoot.ogg,10]
+    ///    - [hit.ogg,10]
+    ///
+    ///music:
+    ///    - village.ogg
+    ///...
+    ///```
+    ///
     pub fn from_yaml(code: &Yaml) -> Self {
         let hash = code.as_hash().expect("config must be an associative array");
         let distance_model = {
@@ -149,191 +184,223 @@ impl Setting {
     }
 }
 
-/// set the volume of sounds effects
-/// take effect for future sounds effects only
-pub fn effect_set_volume(v: f32) {
-    let mut state = STATE.lock().unwrap();
-    state.effect_volume = v;
-}
+pub mod effect {
+    //! this module allow to play sound effect
+    //!
+    //! volume = `global_volume * effect_volume * distance([x,y,z],listener_position)`
+    //!
+    //! but once a sound effect is played at a volume it doesn't change its volume anymore
+    //! this can lead to weird effects for long sound effects
 
-/// get the volume of sounds effects
-pub fn effect_volume() -> f32 {
-    let state = STATE.lock().unwrap();
-    state.effect_volume
-}
+    use super::{STATE, Msg};
 
-/// play the sound effect at the volume: `global_volume * effect_volume *
-/// distance([x,y,z],listener_position)`
-pub fn effect_play(effect: usize, pos: &[f64;3]) {
-    let state = STATE.lock().unwrap();
-    let volume = state.global_volume * state.effect_volume * state.distance_model.distance(pos,&state.listener);
-    if volume > 0. {
-        state.sender.send(Msg::PlayEffect(effect,volume)).unwrap();
+    /// set the volume of sound effects
+    /// take effect for future sounds effects only
+    pub fn set_volume(v: f32) {
+        let mut state = STATE.lock().unwrap();
+        state.effect_volume = v;
     }
-}
 
-/// stop all effects
-pub fn effect_stop_all() {
-    let state = STATE.lock().unwrap();
-    state.sender.send(Msg::StopEffect).unwrap();
-}
+    /// get the volume of sound effects
+    pub fn volume() -> f32 {
+        let state = STATE.lock().unwrap();
+        state.effect_volume
+    }
 
-/// set the position of the listener
-pub fn effect_set_listener(x: f64, y: f64, z: f64) {
-    let mut state = STATE.lock().unwrap();
-    state.listener = [x,y,z];
-}
+    /// play the sound effect at the volume: `global_volume * effect_volume *
+    /// distance([x,y,z],listener_position)`
+    pub fn play(effect: usize, pos: &[f64;3]) {
+        let state = STATE.lock().unwrap();
+        let volume = state.global_volume * state.effect_volume * state.distance_model.distance(pos,&state.listener);
+        if volume > 0. {
+            state.sender.send(Msg::PlayEffect(effect,volume)).unwrap();
+        }
+    }
 
-/// return the position of the listener
-pub fn effect_listener() -> [f64;3] {
-    let state = STATE.lock().unwrap();
-    state.listener
-}
+    /// stop all sound effects
+    pub fn stop_all() {
+        let state = STATE.lock().unwrap();
+        state.sender.send(Msg::StopEffect).unwrap();
+    }
 
-/// set the distance model
-/// take effect for future sounds effects only
-pub fn effect_set_distance_model(d: DistanceModel) {
-    let mut state = STATE.lock().unwrap();
-    state.distance_model = d;
-}
+    /// set the position of the listener
+    pub fn set_listener(x: f64, y: f64, z: f64) {
+        let mut state = STATE.lock().unwrap();
+        state.listener = [x,y,z];
+    }
 
-/// distance model, used to compute sounds effects volume.
-#[derive(Clone,Debug,PartialEq)]
-pub enum DistanceModel {
-    Linear(f64,f64),
-    Pow2(f64,f64),
-}
+    /// return the position of the listener
+    pub fn listener() -> [f64;3] {
+        let state = STATE.lock().unwrap();
+        state.listener
+    }
 
-impl DistanceModel {
-    fn distance(&self, pos: &[f64;3], listener: &[f64;3]) -> f32 {
-        let d = pos.iter()
-            .zip(listener)
-            .map(|(a,b)| (a-b).powi(2))
-            .fold(0.,|sum,i| sum+i)
-            .sqrt();
+    /// set the distance model
+    /// take effect for future sounds effects only
+    pub fn set_distance_model(d: DistanceModel) {
+        let mut state = STATE.lock().unwrap();
+        state.distance_model = d;
+    }
 
-        match *self {
-            DistanceModel::Linear(a,b) => {
-                if d <= a {
-                    1.
-                } else if d <= b {
-                    1. - ((d-a)/(b-a)) as f32
-                } else {
-                    0.
+    /// distance model, used to compute sounds effects volume.
+    #[derive(Clone,Debug,PartialEq)]
+    pub enum DistanceModel {
+        /// if d <= a then 1
+        ///
+        /// if a <= d <= b then 1-((d-a)/(b-a))
+        ///
+        /// if d >= b then 0
+        Linear(f64,f64),
+        /// if d <= a then 1
+        ///
+        /// if a <= d <= b then (1-((d-a)/(b-a)))^2
+        ///
+        /// if d >= b then 0
+        Pow2(f64,f64),
+    }
+
+    impl DistanceModel {
+        fn distance(&self, pos: &[f64;3], listener: &[f64;3]) -> f32 {
+            let d = pos.iter()
+                .zip(listener)
+                .map(|(a,b)| (a-b).powi(2))
+                .fold(0.,|sum,i| sum+i)
+                .sqrt();
+
+            match *self {
+                DistanceModel::Linear(a,b) => {
+                    if d <= a {
+                        1.
+                    } else if d <= b {
+                        1. - ((d-a)/(b-a)) as f32
+                    } else {
+                        0.
+                    }
                 }
-            }
-            DistanceModel::Pow2(a,b) => {
-                if d <= a {
-                    1.
-                } else if d <= b {
-                    (1. - ((d-a)/(b-a)) as f32).powi(2)
-                } else {
-                    0.
+                DistanceModel::Pow2(a,b) => {
+                    if d <= a {
+                        1.
+                    } else if d <= b {
+                        (1. - ((d-a)/(b-a)) as f32).powi(2)
+                    } else {
+                        0.
+                    }
                 }
             }
         }
     }
 }
 
-/// set the volume of the music
-/// the actual music volume is `music_volume * global_volume`
-pub fn music_set_volume(v: f32) {
-    let mut state = STATE.lock().unwrap();
-    state.music_volume = v;
-    state.sender.send(Msg::SetMusicVolume(state.music_volume*state.global_volume)).unwrap();
-}
+pub mod music {
+    //! this module allow to play music
 
-/// get the volume of the music
-pub fn music_volume() -> f32 {
-    let state = STATE.lock().unwrap();
-    state.music_volume
-}
+    use super::{STATE, Msg};
+    use super::sndfile::{SndFile, OpenMode};
 
-/// seek the music to a given frame
-pub fn music_seek(frame: i64) {
-    let state = STATE.lock().unwrap();
-    state.sender.send(Msg::SeekMusic(frame)).unwrap();
-}
+    /// set the volume of the music
+    /// the actual music volume is `music_volume * global_volume`
+    pub fn music_set_volume(v: f32) {
+        let mut state = STATE.lock().unwrap();
+        state.music_volume = v;
+        state.sender.send(Msg::SetMusicVolume(state.music_volume*state.global_volume)).unwrap();
+    }
 
-/// play the music
-pub fn music_play(music: usize) {
-    let mut state = STATE.lock().unwrap();
-    state.music_status.pause = false;
-    state.music_status.id = Some(music);
-    let snd_file = SndFile::new(&state.music[music],OpenMode::Read).unwrap();
-    state.sender.send(Msg::PlayMusic(snd_file)).unwrap();
-}
+    /// get the volume of the music
+    pub fn music_volume() -> f32 {
+        let state = STATE.lock().unwrap();
+        state.music_volume
+    }
 
-/// pause the music
-pub fn music_pause() {
-    let mut state = STATE.lock().unwrap();
-    state.music_status.pause = true;
-    state.sender.send(Msg::PauseMusic).unwrap();
-}
+    /// seek the music to a given frame
+    pub fn music_seek(frame: i64) {
+        let state = STATE.lock().unwrap();
+        state.sender.send(Msg::SeekMusic(frame)).unwrap();
+    }
 
-/// resume the music
-pub fn music_resume() {
-    let mut state = STATE.lock().unwrap();
-    state.music_status.pause = false;
-    state.sender.send(Msg::ResumeMusic).unwrap();
-}
+    /// play the music
+    pub fn music_play(music: usize) {
+        let mut state = STATE.lock().unwrap();
+        state.music_status.pause = false;
+        state.music_status.id = Some(music);
+        let snd_file = SndFile::new(&state.music[music],OpenMode::Read).unwrap();
+        state.sender.send(Msg::PlayMusic(snd_file)).unwrap();
+    }
 
-/// stop the music
-pub fn music_stop() {
-    let mut state = STATE.lock().unwrap();
-    state.music_status.pause = false;
-    state.music_status.id = None;
-    state.sender.send(Msg::StopMusic).unwrap();
-}
+    /// pause the music
+    pub fn music_pause() {
+        let mut state = STATE.lock().unwrap();
+        state.music_status.pause = true;
+        state.sender.send(Msg::PauseMusic).unwrap();
+    }
 
-/// return the current status of the music
-pub fn music_status() -> MusicStatus {
-    let state = STATE.lock().unwrap();
-    state.music_status
-}
+    /// resume the music
+    pub fn music_resume() {
+        let mut state = STATE.lock().unwrap();
+        state.music_status.pause = false;
+        state.sender.send(Msg::ResumeMusic).unwrap();
+    }
 
-/// set whereas music loop or not
-pub fn music_set_loop(l: bool) {
-    let mut state = STATE.lock().unwrap();
-    state.music_status.looping = l;
-    state.sender.send(Msg::SetMusicLoop(l)).unwrap();
-}
+    /// stop the music
+    pub fn music_stop() {
+        let mut state = STATE.lock().unwrap();
+        state.music_status.pause = false;
+        state.music_status.id = None;
+        state.sender.send(Msg::StopMusic).unwrap();
+    }
 
-/// return whereas music loop or not.
-pub fn music_loop() -> bool {
-    let state = STATE.lock().unwrap();
-    state.music_status.looping
-}
+    /// return the current status of the music
+    pub fn music_status() -> MusicStatus {
+        let state = STATE.lock().unwrap();
+        state.music_status
+    }
 
-/// the status of the music
-#[derive(Clone,Copy,Debug,PartialEq)]
-pub struct MusicStatus {
-    id: Option<usize>,
-    pause: bool,
-    looping: bool,
+    /// set whereas music loop or not
+    pub fn music_set_loop(l: bool) {
+        let mut state = STATE.lock().unwrap();
+        state.music_status.looping = l;
+        state.sender.send(Msg::SetMusicLoop(l)).unwrap();
+    }
+
+    /// return whereas music loop or not.
+    pub fn music_loop() -> bool {
+        let state = STATE.lock().unwrap();
+        state.music_status.looping
+    }
+
+    /// the status of the music
+    #[derive(Clone,Copy,Debug,PartialEq)]
+    pub struct MusicStatus {
+        /// the Id of the music played if any
+        pub id: Option<usize>,
+        /// whereas the music is paused
+        pub pause: bool,
+        /// whereas the music is looping
+        pub looping: bool,
+    }
 }
+use music::MusicStatus;
 
 /// set the global volume
-pub fn global_set_volume(v: f32) {
+pub fn set_volume(v: f32) {
     let mut state = STATE.lock().unwrap();
     state.global_volume = v;
     state.sender.send(Msg::SetMusicVolume(state.music_volume*state.global_volume)).unwrap();
 }
 
 /// get the global volume
-pub fn global_volume() -> f32 {
+pub fn volume() -> f32 {
     let state = STATE.lock().unwrap();
     state.global_volume
 }
 
 /// stop music and effects
-pub fn global_stop() {
+pub fn stop() {
     let state = STATE.lock().unwrap();
     state.sender.send(Msg::StopMusic).unwrap();
     state.sender.send(Msg::StopEffect).unwrap();
 }
 
-/// init the audio player using the configuration file in yaml
+/// init the audio player
 pub fn init(setting: Setting) {
     let (sender,receiver) = channel();
     let (abort_sender,abort_receiver) = channel();
@@ -409,6 +476,7 @@ pub fn init(setting: Setting) {
     });
 }
 
+/// close the audio player, it can be init again.
 pub fn close() {
     unsafe {
         if !RAW_STATE.is_null() {
@@ -445,10 +513,14 @@ impl State {
 
         debug_assert!({
             for m in &music {
-                let snd_file = SndFile::new(m,OpenMode::Read).expect("cannot load a music file");
+                let snd_file = SndFile::new(m,OpenMode::Read).expect(&format!("cannot load {:#?}",m));
                 let snd_info = snd_file.get_sndinfo();
-                assert_eq!(snd_info.samplerate as f64,s.sample_rate);
-                assert_eq!(snd_info.channels, s.channels);
+                if snd_info.samplerate as f64 != s.sample_rate {
+                    panic!("samplerate of {:#?} differ from setting",m);
+                }
+                if snd_info.channels != s.channels {
+                    panic!("channels of {:#?} differ from setting",m);
+                }
             }
             true
         });
